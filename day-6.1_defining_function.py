a61454942fe120b7557984a39a1348219777c078
Ranjeet Kumar Jha <117855695+juniorRANJEET@users.noreply.github.com>
2023-02-09 23:17:37 +0530

    Add files via upload

---
+++ day-6.1_defining_function.py
@@ -0,0 +1,9 @@
+def my_function():
+    print("hello Ranjeet")
+    print("you are welcome ...")
+#my_function()   # without calling it can't show any things
+
+def my_function():
+    print("hello Ranjeet")
+    print("you are welcome ...")
+my_function()
---
+++ day-6.2_indentation_function.py
@@ -0,0 +1,11 @@
+def my_function():
+    sky = 0
+    if sky == "clear":  # one indentation takes 4 blank space
+        print("blue")   # two indentation takes 8 blank space
+    elif sky == "cloudy":
+        print("grey")   # blocks inside the indentation belongs to the concern function
+    print('hello')
+my_function()
+print("world")
+# to give one indentation , you can either use 4 times hitting "space key" OR 1 times "tab key"
+# but according to 2017 stack over flow report 4 times hitting space key getting more salary.
